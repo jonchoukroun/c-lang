c5062a6941ce3a2752b5fe9523898de350311b8e
Jon Choukroun <jon.choukroun@twenty20.com>
2017-06-23 15:50:12 -0700

    Replace double spaces with a tab char

---
+++ ch-1/extern_1-21.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define LINELENGTH 81
+#define IN 1
+#define OUT 0
+
+char line[LINELENGTH];
+char new_line[LINELENGTH];
+
+int read_line();
+void replace_spaces();
+
+int main() {
+  int length = 0;
+
+  while ((length = read_line()) > 0) {
+    replace_spaces();
+    printf("%s", new_line);
+  }
+
+  return 0;
+}
+
+int read_line() {
+  int i, c;
+
+  for (i = 0; i < LINELENGTH && (c = getchar()) != EOF && c != '\n'; ++i) {
+    line[i] = c;
+  }
+
+  if (c == '\n') {
+    line[i] = c;
+    ++i;
+  }
+
+  line[i] = '\0';
+
+  return i;
+}
+
+void replace_spaces() {
+  int i, state;
+
+  state = OUT;
+  for (i = 0; i < LINELENGTH; ++i) {
+    if (line[i] == ' ') {
+      if (state == IN) {
+        new_line[i] = '\t';
+        state = OUT;
+      } else {
+        new_line[i] = ' ';
+        state = IN;
+      }
+    } else {
+      if (state == IN) {
+        new_line[i] = line[i];
+        state = OUT;
+      } else {
+        new_line[i] = line[i];
+      }
+    }
+  }
+
+}
