58cc0a88d89f68aab59eae8dbf10e32205e20742
Jon <jonchoukroun@gmail.com>
2017-09-18 07:33:01 -0700

    Retrieve index of value in array

---
+++ algorithms/get_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int get_index(int a, int b[]);
+
+int main() {
+
+  int value = 13;
+  int arr[6] = { 2, 6, 4, 1, 6, 14 };
+
+  printf("Index of value: %d\n", get_index(value, arr));
+
+  return 0;
+}
+
+int get_index(int a, int b[]) {
+
+  for (int i = 0; i < 6; ++i) {
+    if (a == b[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+}
