0feb3cdc6802668552be17fbde65a32115ab6ef8
Jon <jonchoukroun@gmail.com>
2017-09-17 21:23:06 -0700

    Sort hardcoded array in place

--- algorithms/insertion_sort.c
+++ algorithms/insertion_sort.c
@@ -3,9 +3,29 @@
 int main() {
   int arr[6] = { 5, 2, 4, 6, 1, 3 };
 
-  for (int i = 0; i < sizeof(arr); ++i) {
+  int array_size = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < array_size; ++i) {
     printf("%d\t", arr[i]);
   }
+
+  printf("\n");
+  for (int j = 1; j < array_size; ++j) {
+    int key = arr[j];
+    int i = j - 1;
+
+    while (i >= 0 && arr[i] > key) {
+      arr[i + 1] = arr[i];
+      --i;
+    }
+
+    arr[i + 1] = key;
+  }
+
+  for (int i = 0; i < array_size; ++i) {
+    printf("%d\t", arr[i]);
+  }
+  printf("\n");
 
   return 0;
 }
