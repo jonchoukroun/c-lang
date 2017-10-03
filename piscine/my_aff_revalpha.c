8180ec00e4e9ae9aa6e64c213d0c86fbe6252e23
Jon Choukroun <jon.choukroun@twenty20.com>
2017-10-03 10:29:49 -0700

    Call worker function from main

--- piscine/my_aff_revalpha.c
+++ piscine/my_aff_revalpha.c
@@ -1,6 +1,14 @@
 #include <unistd.h>
 
+int my_aff_revalpha();
+
 int main() {
+  my_aff_revalpha();
+
+  return 0;
+}
+
+int my_aff_revalpha() {
   char c = 'z';
   while(c >= 'a') {
     write(0, &c, 1);
