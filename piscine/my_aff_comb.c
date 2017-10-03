f2f2480def97a5c88918d6edf6afb4f8c4db063b
Jon Choukroun <jon.choukroun@twenty20.com>
2017-10-03 15:25:32 -0700

    Fix comma repeat

--- piscine/my_aff_comb.c
+++ piscine/my_aff_comb.c
@@ -11,7 +11,7 @@ int main() {
 int my_aff_comb() {
   char a, b, c;
 
-  for(a = '0'; a <= '9'; ++a) {
+  for(a = '0'; a < '9'; ++a) {
     b = a + 1;
 
     while(b <= '9') {
@@ -21,7 +21,10 @@ int my_aff_comb() {
         write(0, &a, 1);
         write(0, &b, 1);
         write(0, &c, 1);
-        write(0, "\n", 1);
+
+        if (a != '7') {
+          write(0, ", ", 2);
+        }
 
         ++c;
       }
